edeeddc81d491d2e7d873dd88d1bfec0347906a2
movingpictures83 <movingpictures83@gmail.com>
2020-08-21 07:22:14 -0400

    Added 'eq' output

--- testbench.v
+++ testbench.v
@@ -4,8 +4,9 @@ reg[31:0] A;
 reg[31:0] B;
 reg[4:0] alu_op;
 wire[31:0] S;
+wire eq;
 
-ALU32 myALU(A, alu_op, B, S);
+ALU32 myALU(A, alu_op, B, eq, S);
 
 initial begin
 A=45; alu_op=5'b00000; B=21;   #10; // AND 
@@ -24,6 +25,9 @@ alu_op=5'b01110; #10; // SUB
 if (S !== (A-B)) begin
            $display("Error computing %d-%d: Should be %d but got %d", A, B, A-B, S); $stop;
 end
+if (eq !== 0) begin
+   $display("Unequal operands did not yield eq of 0.  Value: $d", eq);
+end
 alu_op=5'b01111; #10; // SLT
 if (S !== (A<B)) begin
            $display("Error computing %d<%d: Should be %d but got %d", A, B, A<B, S); $stop;
@@ -40,7 +44,10 @@ A=21; B=45; alu_op=5'b11001; #10; // NAND
 if (S !== (~(A&B))) begin
            $display("Error computing %d NAND %d: Should be %d but got %d", A, B, ~(A&B), S); $stop;
 end
-
+A=45; B=45; alu_op=5'b01110; #10; // EQUALITY CHECK
+if (eq !== 1) begin
+   $display("Equal operands did not yield eq of 1.  Value: $d", eq);
+end
 $display("All tests passed.");
 end
 
