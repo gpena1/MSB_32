cf0a4dea4c955d59a35ebceda6f72ed914392ad9
movingpictures83 <movingpictures83@gmail.com>
2021-02-05 05:49:18 -0500

    Added Grounder

--- ALUBit31.v
+++ ALUBit31.v
@@ -1,21 +1,20 @@
-// Copyright (C) 1991-2015 Altera Corporation. All rights reserved.
-// Your use of Altera Corporation's design tools, logic functions 
+// Copyright (C) 2018  Intel Corporation. All rights reserved.
+// Your use of Intel Corporation's design tools, logic functions 
 // and other software and tools, and its AMPP partner logic 
 // functions, and any output files from any of the foregoing 
 // (including device programming or simulation files), and any 
 // associated documentation or information are expressly subject 
-// to the terms and conditions of the Altera Program License 
-// Subscription Agreement, the Altera Quartus II License Agreement,
-// the Altera MegaCore Function License Agreement, or other 
-// applicable license agreement, including, without limitation, 
-// that your use is for the sole purpose of programming logic 
-// devices manufactured by Altera and sold by Altera or its 
-// authorized distributors.  Please refer to the applicable 
-// agreement for further details.
+// to the terms and conditions of the Intel Program License 
+// Subscription Agreement, the Intel Quartus Prime License Agreement,
+// the Intel FPGA IP License Agreement, or other applicable license
+// agreement, including, without limitation, that your use is for
+// the sole purpose of programming logic devices manufactured by
+// Intel and sold by Intel or its authorized distributors.  Please
+// refer to the applicable agreement for further details.
 
-// PROGRAM		"Quartus II 64-Bit"
-// VERSION		"Version 15.0.0 Build 145 04/22/2015 SJ Web Edition"
-// CREATED		"Tue Feb  2 07:22:48 2021"
+// PROGRAM		"Quartus Prime"
+// VERSION		"Version 18.1.0 Build 625 09/12/2018 SJ Lite Edition"
+// CREATED		"Fri Feb 05 05:36:56 2021"
 
 module ALUBit31(
 	a,
@@ -49,6 +48,7 @@ wire	B_afterMUX;
 wire	notA;
 wire	notB;
 wire	S;
+wire	toGround;
 
 
 
@@ -61,6 +61,10 @@ MUX4	b2v_finalMUX(
 	.D(less),
 	.S(Operation),
 	.Y(Result));
+
+
+Grounder	b2v_inst(
+	.Input_To_Ground(toGround));
 
 assign	notA =  ~a;
 
@@ -89,7 +93,7 @@ OneBitAdder	b2v_myAdder(
 	.ci(CarryIn),
 	.a(A_afterMUX),
 	.b(B_afterMUX),
-	
+	.co(toGround),
 	.s(S));
 
 
